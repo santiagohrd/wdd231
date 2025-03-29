85c9192777df0ad67204cfea986ed97bbd93fdd9
SantiagoHrd <santiagohrd1@gmail.com>
2025-03-29 13:36:51 -0500

    Create the lsit of membership levels, and the cards to save them

--- chamber/scripts/membership.js
+++ chamber/scripts/membership.js
@@ -1,6 +1,7 @@
 const memberships = [
     {
         level: 'Gold',
+        price: '30',
         benefits: [
             'Business consulting',
             'Data and Resources',
@@ -8,24 +9,88 @@ const memberships = [
             'Event discounts',
             'Entrepreneur Networking',
             'Legal advice'
-        ],
+        ]
+    },
+    {
         level: 'Silver',
+        price: '20',
         benefits: [
             'Business consulting',
             'Data and Resources',
             'Trainings',
             'Event discounts',
             'Entrepreneur Networking'
-        ],
+        ]
+    },
+    {
         level: 'Bronze',
+        price: '10',
         benefits: [
             'Business consulting',
             'Data and Resources',
             'Trainings'
-        ],
-        level: 'NP Membership',
+        ]
+    },
+    {
+        level: 'Non Profit',
+        price: 'Free',
         benefits: [
             'Business consulting'
         ]
     }
-]
+];
+
+const membershipContainer = document.querySelector(".membership-list");
+const membershipDetails = document.querySelector('#membership-details');
+
+
+function displayMemberships(membreshipList) {
+    membershipContainer.innerHTML = "";
+
+    let memberList = document.createElement('h3');
+    memberList.textContent = `Membership Levels`;
+    membershipContainer.appendChild(memberList);
+
+    membreshipList.forEach(level => {
+        let card = document.createElement("div");
+        card.className = "membership-level";
+
+        let title = document.createElement('h4');
+        let bntMembership = document.createElement('button');
+
+        title.textContent = `${level.level} Membership Level`;
+        bntMembership.innerHTML = `Learn more`;
+
+        
+
+        membershipContainer.appendChild(card);
+        card.append(title);
+        card.append(bntMembership);
+
+        card.addEventListener('click', () => {
+            displayMembershipDetail(level);
+        });
+    });
+}
+
+function displayMembershipDetail(memberships) {
+    let benefitsList = `<ul>`;
+    memberships.benefits.forEach(benefit => {
+        benefitsList += `<li>${benefit}</li>`;
+    });
+    benefitsList += `</ul>`;
+
+    membershipDetails.innerHTML = `
+        <button id="closeModal">❌</button>
+        <h2>${memberships.level} $${memberships.price}</h2>
+        ${benefitsList} 
+    `;
+
+    membershipDetails.showModal();
+
+    document.getElementById("closeModal").addEventListener("click", () => {
+        membershipDetails.close();
+    });
+}
+
+displayMemberships(memberships);
