745a3a316687dcd709312ed44213b285f4b2348e
SantiagoHrd <santiagohrd1@gmail.com>
2025-03-29 11:18:56 -0500

    Create the membership level and its benefifs

---
+++ chamber/scripts/membership.js
@@ -0,0 +1,31 @@
+const memberships = [
+    {
+        level: 'Gold',
+        benefits: [
+            'Business consulting',
+            'Data and Resources',
+            'Trainings',
+            'Event discounts',
+            'Entrepreneur Networking',
+            'Legal advice'
+        ],
+        level: 'Silver',
+        benefits: [
+            'Business consulting',
+            'Data and Resources',
+            'Trainings',
+            'Event discounts',
+            'Entrepreneur Networking'
+        ],
+        level: 'Bronze',
+        benefits: [
+            'Business consulting',
+            'Data and Resources',
+            'Trainings'
+        ],
+        level: 'NP Membership',
+        benefits: [
+            'Business consulting'
+        ]
+    }
+]
